ba4d9cc6e3cface7b790eeea195fd51a1f106257
Dave Gill <david.g.h.gill@gmail.com>
2021-07-24 13:36:39 +0100

    Added an overflow trap

---
+++ trapoverflow.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <limits.h>
+#include <setjmp.h>
+
+static jmp_buf jumpBuffer;
+
+void signalHandler(int sig)
+{
+    printf("Overflow detected\n");
+    siglongjmp(jumpBuffer, 1);
+}
+
+int main(int argc, char ** argv){
+    sigset_t mask;
+    sigfillset(&mask);
+    struct sigaction psa={0};
+    psa.sa_handler = signalHandler;
+    psa.sa_mask = mask;
+
+    int a = INT_MAX;
+    int b = INT_MAX;
+    if (sigsetjmp(jumpBuffer, 0)){
+        printf("Overflow\n");
+        printf("%d \n", a);
+    } else {
+        sigaction(SIGABRT, &psa, NULL);
+        int result = a + b;
+        printf("%d \n", result);
+    }
+    return (EXIT_SUCCESS);
+}
